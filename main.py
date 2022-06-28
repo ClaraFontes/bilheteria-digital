73e96a165d3cfeaf1e8b979a10889f727496e9d0
Ana <clara.fontes.ac@gmail.com>
2022-06-28 15:01:34 -0300

    primeira conexão de telas.

--- main.py
+++ main.py
@@ -1,14 +1,23 @@
-from PyQt6.QtWidgets import QApplication, QWidget
-from PyQt6 import uic, QtWidgets 
-
-def func_teste():
-    print('botao funcionando')
+#from PyQt6.QtWidgets import QApplication, QWidget
+from PyQt6 import uic, QtWidgets
 
 app = QtWidgets.QApplication([])
+# importando as telas.
 inicio = uic.loadUi('./telas/tela_inicio.ui')
-inicio.bt_vender.clicked.connect(func_teste)
+loginAdm = uic.loadUi('./telas/tela_loginAdm.ui')
+loginCliente = uic.loadUi('./telas/tela_loginCliente.ui')
 
+def func_abrirloginAdm():
+    inicio.hide()
+    loginAdm.show()
 
+def func_abrirloginCliente():
+    inicio.hide()
+    loginCliente.show()
+
+inicio.bt_vender.clicked.connect(func_abrirloginAdm) # abre login adm.
+inicio.bt_comprar.clicked.connect(func_abrirloginCliente) # abre login cliente.
+    
 inicio.show()
 app.exec()
 
